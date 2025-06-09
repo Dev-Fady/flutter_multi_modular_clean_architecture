683d572a47f273b8faf5fab09117d852e0a96d9e
fady emil <fadymobiledev@gmail.com>
2025-06-09 18:06:46 +0300

    Refactor data.dart and login.dart to ensure proper Dio import and usage

--- features/auth/login/lib/login.dart
+++ features/auth/login/lib/login.dart
@@ -1,5 +1,12 @@
+#! Not the best way
+// import 'package:data/data.dart';
+
+
+import 'package:dio/dio.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  Dio dio = Dio();
 }
