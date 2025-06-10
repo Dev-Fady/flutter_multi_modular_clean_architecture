e5a76aa6ea7c03b3bac88d45daccc96720d85296
fady emil <fadymobiledev@gmail.com>
2025-06-10 17:59:11 +0300

    Add main provider with app name and version functions; update profile and pubspec.yaml

--- features/settings/profile/lib/profile.dart
+++ features/settings/profile/lib/profile.dart
@@ -1,5 +1,10 @@
+import 'package:flutter_multi_modular_clean_architecture/main_provider.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+ var name = getAppName();
+ var version = getAppVersion();
 }
---
+++ lib/main_provider.dart
@@ -0,0 +1,7 @@
+String getAppName() {
+  return 'Flutter Multi Modular Clean Architecture';
+}
+
+String getAppVersion() {
+  return '1.0.0';
+}
