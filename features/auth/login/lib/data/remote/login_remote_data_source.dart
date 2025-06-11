639fe375ce3f056977268edf53dbb09e4985de6b
fady emil <fadymobiledev@gmail.com>
2025-06-11 15:53:33 +0300

    Add LoginRemoteDataSource and LoginRemoteDataSourceImpl for handling login requests

---
+++ features/auth/login/lib/data/remote/login_remote_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:login/data/request/login_request.dart';
+import 'package:login/data/response/login_response.dart';
+import 'package:retrofit/retrofit.dart';
+
+abstract class LoginRemoteDataSource {
+  Future<HttpResponse<LoginResponse>> login({
+    required LoginRequest loginRequest,
+  });
+}
