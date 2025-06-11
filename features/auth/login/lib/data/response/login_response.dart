9a24a650c9a198ddffc0e853c64609f8c4b9c6bf
fady emil <fadymobiledev@gmail.com>
2025-06-11 15:11:15 +0300

    Add LoginResponse class with JSON serialization support

--- features/auth/login/lib/data/response/login_response.dart
+++ features/auth/login/lib/data/response/login_response.dart
@@ -1,1 +1,28 @@
-class LoginResponse {}
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_response.g.dart';
+
+@JsonSerializable()
+class LoginResponse {
+  @JsonKey(name: "name")
+  String? name;
+
+  @JsonKey(name: "phone")
+  String? phone;
+
+  @JsonKey(name: "age")
+  String? age;
+
+  LoginResponse({
+    this.name,
+    this.phone,
+    this.age,
+  });
+
+  // from json
+  factory LoginResponse.fromJson(Map<String, dynamic> json) =>
+      _$LoginResponseFromJson(json);
+
+      // to json
+  Map<String, dynamic> toJson() => _$LoginResponseToJson(this);
+}
