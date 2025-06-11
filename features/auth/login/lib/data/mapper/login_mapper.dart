f40be63f9dfcffb4063d3ea65495dba26c36ced2
fady emil <fadymobiledev@gmail.com>
2025-06-11 18:08:49 +0300

    Refactor LoginResponseMapper to handle nullable fields and use constants for default values

--- features/auth/login/lib/data/mapper/login_mapper.dart
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -1,13 +1,15 @@
+import 'package:extensions/constant/constant.dart';
+import 'package:extensions/extensions.dart';
 import 'package:login/domain/model/login_model.dart';
 
 import '../response/login_response.dart';
 
-extension loginResponseMapper on LoginResponse {
+extension LoginResponseMapper on LoginResponse? {
   LoginModel toDomain() {
     return LoginModel(
-      name: name ?? 'nullName',
-      phone: phone ?? 'NullPhone',
-      age: age ?? 'NullAge',
+      name: this?.name.orEmpty() ?? Constant.empty,
+      phone: this?.phone.orEmpty() ?? Constant.empty,
+      age: this?.age.orEmpty() ?? Constant.empty,
     );
   }
 }
