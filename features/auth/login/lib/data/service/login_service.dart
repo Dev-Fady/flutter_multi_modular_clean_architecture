39582e0a2c04b7e011ff79f45aa0e798a17f9788
fady emil <fadymobiledev@gmail.com>
2025-06-11 15:19:36 +0300

    Add error logger support to LoginService factory constructor

--- features/auth/login/lib/data/service/login_service.dart
+++ features/auth/login/lib/data/service/login_service.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:login/data/response/login_response.dart';
 import 'package:retrofit/dio.dart';
+import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
-import 'package:retrofit/retrofit.dart';
 
 part 'login_service.g.dart';
 
@@ -10,7 +10,8 @@ const String baseUrl = "https://minafarid.mocklab.io";
 
 @RestApi(baseUrl: baseUrl)
 abstract class LoginService {
-  factory LoginService(Dio dio, {String baseUrl}) = _LoginService;
+  factory LoginService(Dio dio,
+      {String baseUrl, ParseErrorLogger errorLogger}) = _LoginService;
 
   @POST("/customers/login")
   Future<HttpResponse<LoginResponse>> login(
