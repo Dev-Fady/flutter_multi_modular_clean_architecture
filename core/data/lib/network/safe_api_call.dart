cdd21e437afc9c43a56b656ce84e8a724e0ded73
fady emil <fadymobiledev@gmail.com>
2025-06-12 14:47:40 +0300

    Refactor safeApiCall to include network connectivity checks

--- core/data/lib/network/safe_api_call.dart
+++ core/data/lib/network/safe_api_call.dart
@@ -1,17 +1,29 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localised_message.dart';
 
-Future<Either<Failure, T>> safeApiCall<T>(Future<T> Function() apiCall) async {
-  try {
-    final result = await apiCall();
-    return Right(result);
-  } catch (error) {
+Future<Either<Failure, T>> safeApiCall<T>(
+    NetworkInfo networkInfo, Future<T> Function() apiCall) async {
+  if (await networkInfo.isConnected) {
+    try {
+      final result = await apiCall();
+      return Right(result);
+    } catch (error) {
+      return Left(
+        Failure(
+          code: error.hashCode,
+          message: LocalisedMessage('english', 'arabic'),
+        ),
+      ); //todo handle error
+    }
+  } else {
     return Left(
       Failure(
-        code: error.hashCode,
-        message: LocalisedMessage('english', 'arabic'),
+        code: -500,
+        message: LocalisedMessage(
+            'en No internet connection ', ' ar No internet connection'),
       ),
-    ); //todo handle error
+    );
   }
 }
