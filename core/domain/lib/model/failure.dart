05887614bdc86e00d11d7c2a5523d3b93e0e4b4d
fady emil <fadymobiledev@gmail.com>
2025-06-12 16:57:06 +0300

    Refactor Failure class to use String for message instead of LocalisedMessage

--- core/domain/lib/model/failure.dart
+++ core/domain/lib/model/failure.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:domain/model/localised_message.dart';
+// import 'package:domain/model/localised_message.dart';
 
 class Failure {
   int code;
-  LocalisedMessage message;
+  String message;
   Failure({
     required this.code,
     required this.message,
