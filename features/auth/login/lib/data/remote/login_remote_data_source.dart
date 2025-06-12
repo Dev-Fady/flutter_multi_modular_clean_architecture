0512d721aadb7d8e13144e45d75ea94de0d923c2
fady emil <fadymobiledev@gmail.com>
2025-06-12 14:19:16 +0300

    Refactor LoginRemoteDataSource to use Either for login response handling

--- features/auth/login/lib/data/remote/login_remote_data_source.dart
+++ features/auth/login/lib/data/remote/login_remote_data_source.dart
@@ -1,9 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:domain/model/failure.dart';
 import 'package:login/data/request/login_request.dart';
 import 'package:login/data/response/login_response.dart';
-import 'package:retrofit/retrofit.dart';
 
 abstract class LoginRemoteDataSource {
-  Future<HttpResponse<LoginResponse>> login({
+  Future<Either<Failure, LoginResponse>> login({
     required LoginRequest loginRequest,
   });
 }
--- features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
+++ features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
@@ -1,18 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:data/network/safe_api_call.dart';
+import 'package:domain/model/failure.dart';
 import 'package:login/data/remote/login_remote_data_source.dart';
 import 'package:login/data/request/login_request.dart';
 import 'package:login/data/response/login_response.dart';
 import 'package:login/data/service/login_service.dart';
-import 'package:retrofit/dio.dart';
+// import 'package:retrofit/dio.dart';
 
 class LoginRemoteDataSourceImpl extends LoginRemoteDataSource {
   final LoginService loginService;
   LoginRemoteDataSourceImpl({required this.loginService});
+
   @override
-  Future<HttpResponse<LoginResponse>> login(
-      {required LoginRequest loginRequest}) async {
-    return await loginService.login(
-      loginRequest.email,
-      loginRequest.password,
+  Future<Either<Failure, LoginResponse>> login(
+      {required LoginRequest loginRequest}) {
+    return safeApiCall(
+      () async {
+        final response =
+            await loginService.login(loginRequest.email, loginRequest.password);
+        return response.data;
+      },
     );
   }
 }
