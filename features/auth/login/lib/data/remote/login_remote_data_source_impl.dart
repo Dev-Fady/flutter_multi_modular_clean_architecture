bf0261ddf36fd4581f13c3d11c4deff72e9e5149
fady emil <fadymobiledev@gmail.com>
2025-06-12 14:47:56 +0300

    Refactor LoginRemoteDataSourceImpl to include NetworkInfo for enhanced network checks

--- features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
+++ features/auth/login/lib/data/remote/login_remote_data_source_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:data/network/safe_api_call.dart';
+import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:login/data/remote/login_remote_data_source.dart';
 import 'package:login/data/request/login_request.dart';
@@ -9,12 +10,14 @@ import 'package:login/data/service/login_service.dart';
 
 class LoginRemoteDataSourceImpl extends LoginRemoteDataSource {
   final LoginService loginService;
-  LoginRemoteDataSourceImpl({required this.loginService});
+  final NetworkInfo networkInfo;
+  LoginRemoteDataSourceImpl({required this.loginService, required this.networkInfo});
 
   @override
   Future<Either<Failure, LoginResponse>> login(
       {required LoginRequest loginRequest}) {
     return safeApiCall(
+      networkInfo,
       () async {
         final response =
             await loginService.login(loginRequest.email, loginRequest.password);
