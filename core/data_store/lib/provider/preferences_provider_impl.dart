8b8ce8fdf847ab6bf1d438977f20d7857108ba55
fady emil <fadymobiledev@gmail.com>
2025-06-12 17:54:08 +0300

    Implement PreferencesProvider and PreferencesProviderImpl for managing app language and base URL

---
+++ core/data_store/lib/provider/preferences_provider_impl.dart
@@ -0,0 +1,32 @@
+
+import 'package:data_store/provider/pref_strings.dart';
+import 'package:data_store/provider/preferences_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesProviderImpl implements PreferencesProvider {
+  final SharedPreferences _sharedPreferences;
+
+  PreferencesProviderImpl(this._sharedPreferences);
+
+  @override
+  String getAppLanguage() {
+    return _sharedPreferences.getString(PreferencesStrings.appLanguageKey) ??
+        PreferencesStrings.appLanguageDefault;
+  }
+
+  @override
+  void setAppLanguage(String language) {
+    _sharedPreferences.setString(PreferencesStrings.appLanguageKey, language);
+  }
+
+  @override
+  String getBaseUrl() {
+    return _sharedPreferences.getString(PreferencesStrings.baseUrlKey) ??
+        PreferencesStrings.baseUrlDefault;
+  }
+
+  @override
+  void setBaseUrl(String baseUrl) {
+    _sharedPreferences.setString(PreferencesStrings.baseUrlKey, baseUrl);
+  }
+}
