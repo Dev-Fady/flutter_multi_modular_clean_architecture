2d968ac327571fc7f9ffb2bd23d3b6970da246ba
fady emil <fadymobiledev@gmail.com>
2025-06-12 16:57:12 +0300

    Refactor safeApiCall to use ErrorHandler for exception handling and remove LocalisedMessage dependency

--- core/data/lib/network/safe_api_call.dart
+++ core/data/lib/network/safe_api_call.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/error_handler/data_source.dart';
+import 'package:data/error_handler/data_source_extension.dart';
+import 'package:data/error_handler/dio_error_hanlder.dart';
 import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
-import 'package:domain/model/localised_message.dart';
-
 Future<Either<Failure, T>> safeApiCall<T>(
     NetworkInfo networkInfo, Future<T> Function() apiCall) async {
   if (await networkInfo.isConnected) {
@@ -10,20 +11,9 @@ Future<Either<Failure, T>> safeApiCall<T>(
       final result = await apiCall();
       return Right(result);
     } catch (error) {
-      return Left(
-        Failure(
-          code: error.hashCode,
-          message: LocalisedMessage('english', 'arabic'),
-        ),
-      ); //todo handle error
+      return Left(ErrorHandler.handle(error).failure);
     }
   } else {
-    return Left(
-      Failure(
-        code: -500,
-        message: LocalisedMessage(
-            'en No internet connection ', ' ar No internet connection'),
-      ),
-    );
+    return Left(DataSource.notInternetConnection.getFailure());
   }
 }
