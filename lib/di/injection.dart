f99c8f4c59db6913229f587923d21f37c0f4a237
fady emil <fadymobiledev@gmail.com>
2025-06-13 12:36:44 +0300

    Refactor dependency configuration to use async/await and include core data dependencies

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,4 +1,5 @@
 import 'package:app_settings/di/injection.dart';
+import 'package:data/di/injection.dart';
 import 'package:data_store/di/injection.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_multi_modular_clean_architecture/di/injection.config.dart';
@@ -7,8 +8,9 @@ import 'package:injectable/injectable.dart';
 final getIt = GetIt.instance;
 
 @InjectableInit()
-void configureDependencies(String? environment) {
+Future<void> configureDependencies(String? environment) async {
   getIt.init(environment: environment);
-  configureAppSettingsDependencies(getIt, environment);
-  configureDataStoreDependencies(getIt, environment);
+  await configureAppSettingsDependencies(getIt, environment);
+  await configureDataStoreDependencies(getIt, environment);
+  await configureCoreDataDependencies(getIt, environment);
 }
