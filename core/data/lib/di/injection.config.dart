c6ad3281220925b8a1e2b9b392a7be7ef6b9ce1f
fady emil <fadymobiledev@gmail.com>
2025-06-13 12:36:20 +0300

    Add injection configuration for dependency registration in GetIt

---
+++ core/data/lib/di/injection.config.dart
@@ -0,0 +1,54 @@
+// dart format width=80
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:data/di/data_module.dart' as _i202;
+import 'package:data/network_info/network_info.dart' as _i131;
+import 'package:data_store/provider/preferences_provider.dart' as _i812;
+import 'package:data_store/provider/session_provider.dart' as _i687;
+import 'package:dio/dio.dart' as _i361;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final dataModule = _$DataModule();
+    gh.lazySingleton<_i131.NetworkInfo>(() => dataModule.provideNetworkInfo());
+    gh.factory<String>(
+      () => dataModule.provideAccessToken(gh<_i687.SessionProvider>()),
+      instanceName: 'AccessToken',
+    );
+    gh.factory<String>(
+      () => dataModule.provideBaseUrl(gh<_i812.PreferencesProvider>()),
+      instanceName: 'BaseUrl',
+    );
+    gh.factory<String>(
+      () => dataModule.provideLanguage(gh<_i812.PreferencesProvider>()),
+      instanceName: 'Language',
+    );
+    gh.lazySingleton<_i361.Dio>(() => dataModule.dio(
+          gh<String>(instanceName: 'BaseUrl'),
+          gh<String>(instanceName: 'AccessToken'),
+          gh<String>(instanceName: 'Language'),
+        ));
+    return this;
+  }
+}
+
+class _$DataModule extends _i202.DataModule {}
