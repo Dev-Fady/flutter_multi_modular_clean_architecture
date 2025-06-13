69bb3103c44dfa9f2cc8f38769b5990974d3268b
fady emil <fadymobiledev@gmail.com>
2025-06-13 12:35:59 +0300

    Refactor ProdSessionImplProvider and DevSessionImplProvider to use SharedPreferences for accessToken, refreshToken, and userId management

--- core/data_store/lib/provider/session_impl_provider.dart
+++ core/data_store/lib/provider/session_impl_provider.dart
@@ -1,21 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:data_store/provider/session_provider.dart';
+import 'package:data_store/provider/session_strings.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 @Injectable(as: SessionProvider, env: [Environment.prod])
 class ProdSessionImplProvider extends SessionProvider {
+  final SharedPreferences _sharedPreferences;
+  ProdSessionImplProvider(this._sharedPreferences);
+
   @override
   String getAccessToken() {
-    return 'access_token_prod';
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setAccessToken(String accessToken) {
+    _sharedPreferences.setString(SessionStrings.accessTokenKey, accessToken);
   }
 
   @override
   String getClientId() {
-    return 'client_id_prod';
+    return Uuid().v4();
   }
 
   @override
   String getRefreshToken() {
-    return 'refresh_token_prod';
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setRefreshToken(String refreshToken) {
+    _sharedPreferences.setString(SessionStrings.refreshTokenKey, refreshToken);
   }
 
   @override
@@ -25,34 +42,58 @@ class ProdSessionImplProvider extends SessionProvider {
 
   @override
   String getUserId() {
-    return 'user_id_prod';
+    return _sharedPreferences.getString(SessionStrings.userIdKey) ?? "";
+  }
+
+  @override
+  void setUserId(String userId) {
+    _sharedPreferences.setString(SessionStrings.userIdKey, userId);
   }
 }
 
 @Injectable(as: SessionProvider, env: [Environment.dev])
 class DevSessionImplProvider extends SessionProvider {
+  final SharedPreferences _sharedPreferences;
+
+  DevSessionImplProvider(this._sharedPreferences);
+
   @override
   String getAccessToken() {
-    return 'access_token_dev';
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setAccessToken(String accessToken) {
+    _sharedPreferences.setString(SessionStrings.accessTokenKey, accessToken);
   }
 
   @override
   String getClientId() {
-    return 'client_id_dev';
+    return Uuid().v4();
   }
 
   @override
   String getRefreshToken() {
-    return 'refresh_token_dev';
+    return _sharedPreferences.getString(SessionStrings.accessTokenKey) ?? "";
+  }
+
+  @override
+  void setRefreshToken(String refreshToken) {
+    _sharedPreferences.setString(SessionStrings.refreshTokenKey, refreshToken);
+  }
+
+  @override
+  String getUserId() {
+    return _sharedPreferences.getString(SessionStrings.userIdKey) ?? "";
+  }
+
+  @override
+  void setUserId(String userId) {
+    _sharedPreferences.setString(SessionStrings.userIdKey, userId);
   }
 
   @override
   String getSessionId() {
     return 'session_id_dev';
-  }
-
-  @override
-  String getUserId() {
-    return 'user_id_dev';
   }
 }
