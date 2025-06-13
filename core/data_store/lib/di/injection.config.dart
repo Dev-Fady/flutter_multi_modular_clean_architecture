012fc15ab47735d090fab443ac9278ffa8a2bf16
fady emil <fadymobiledev@gmail.com>
2025-06-13 12:35:35 +0300

    Enhance dependency injection by adding SharedPreferences to session providers and updating configureDataStoreDependencies to be asynchronous

--- core/data_store/lib/di/injection.config.dart
+++ core/data_store/lib/di/injection.config.dart
@@ -13,6 +13,7 @@ import 'package:data_store/provider/session_impl_provider.dart' as _i681;
 import 'package:data_store/provider/session_provider.dart' as _i687;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
 
 const String _dev = 'dev';
 const String _prod = 'prod';
@@ -29,11 +30,11 @@ extension GetItInjectableX on _i174.GetIt {
       environmentFilter,
     );
     gh.factory<_i687.SessionProvider>(
-      () => _i681.DevSessionImplProvider(),
+      () => _i681.DevSessionImplProvider(gh<_i460.SharedPreferences>()),
       registerFor: {_dev},
     );
     gh.factory<_i687.SessionProvider>(
-      () => _i681.ProdSessionImplProvider(),
+      () => _i681.ProdSessionImplProvider(gh<_i460.SharedPreferences>()),
       registerFor: {_prod},
     );
     return this;
